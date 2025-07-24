852ccf0b1add806190af217d459ddd12536e3775
quartzs2 <csh001231@gmail.com>
2025-07-24 12:22:23 +0900

    chore: suit 폰트 적용 및 tsconfig 경로 별칭 수정(SCENTLY-7)
    - next/font/local을 이용해 SUIT 폰트를 프로젝트에 추가하고, layout.tsx에서 전역으로 적용했습니다.
    - tsconfig.json에 baseUrl을 추가하고 paths를 간소화하여 모듈 임포트 경로를 개선했습니다.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,21 @@
 import type { Metadata } from "next";
 
-import "./globals.css";
+import localFont from "next/font/local";
+import "@app/globals.css";
 
 export const metadata: Metadata = {
   description: "취향에 맞는 향수를 추천해주는 향수 쇼핑몰",
   title: "Yours, Scently",
 };
+
+const suit = localFont({
+  src: [
+    {
+      path: "../assets/fonts/SUIT-Variable.woff2",
+      weight: "100 900",
+    },
+  ],
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html className={suit.className} lang="en">
       <body>{children}</body>
     </html>
   );
