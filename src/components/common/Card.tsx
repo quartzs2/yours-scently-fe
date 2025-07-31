670b3b3a2a9c6c18b3b99a543f6417ca93687af4
dideksql <didrudah23@naver.com>
2025-07-31 15:02:30 +0900

    fix: card 코드 작성 변경(SCENTLY-43)
    - card 태그 컴포넌트 추가 됐을때 TODO 주석추가.
    - IconButton 컴포넌트로 변경.
    - Image 컴포넌트로 변경.
    - key 변경
    - public에 fallback-image 추가

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import IconButton from "@components/ui/IconButton";
 import { Heart } from "lucide-react";
+import { useState } from "react";
+import Image from "next/image";
 
 export type CardProps = {
   imageUrl?: string;
@@ -9,31 +12,43 @@ export type CardProps = {
   name: string;
 };
 
+const ICON_HEART_STYLE = "absolute top-2 right-2 border-primary-main";
+const FALLBACK_IMAGE = "/fallback-image.svg";
+
 const Card = ({ tags = [], imageUrl, price, name }: CardProps) => {
+  const [imgSrc, setImgSrc] = useState(imageUrl);
+  const isFallback = FALLBACK_IMAGE;
+
   return (
     <div className="w-[250px]">
       {/* 이미지 & 하트 */}
       <div className="relative mb-2 flex h-[250px] w-full items-center justify-center overflow-hidden rounded-lg border border-border-default">
-        {imageUrl ? (
-          <img
-            className="h-full w-full object-cover"
-            src={imageUrl}
-            alt={name}
-          />
-        ) : (
-          <span className="text-sm">이미지 없음</span>
-        )}
-        <Heart className="absolute top-2 right-2 cursor-pointer border-primary-main" />
+        <Image
+          style={{ objectFit: isFallback ? "contain" : "cover" }}
+          onError={() => setImgSrc(FALLBACK_IMAGE)}
+          src={imgSrc ?? "/fallback-image.svg"}
+          height={isFallback ? 80 : undefined}
+          width={isFallback ? 80 : undefined}
+          fill={!isFallback}
+          alt={name}
+        />
+
+        <IconButton
+          iconClassName={ICON_HEART_STYLE}
+          aria-label="좋아요 버튼"
+          As={Heart}
+        />
       </div>
 
       {/* 카드 하단 부분 */}
       <div className="rounded-lg border border-border-default">
         {/* 태그 */}
+        {/* TODO: 추후 태그 컴포넌트로 변경*/}
         <div className="flex flex-wrap gap-1 px-3 py-2">
           {tags.map((tag, i) => (
             <span
               className="text-caption rounded-full border border-primary-main px-2 py-1"
-              key={i}
+              key={`${tag}-${i}`}
             >
               {tag}
             </span>
